13f2062d33997eb5be3cd5adc27527c47608f959
Hugo Ingeniate <soporte2@ingeniate.com.mx>
2025-01-29 09:45:46 -0600

    change environment variables of database

--- config/db.js
+++ config/db.js
@@ -3,12 +3,12 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '.env' });
 
 const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
     dialect: 'mysql',
     define: {
       timestamps: true,
